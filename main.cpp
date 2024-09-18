8e7b42c72581f8788c23689b71ada57fb065ac86
MizzenMastodon <Nbaum0701@gmail.com>
2024-09-18 17:15:57 +0000

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main() { 
-  float milesDriven = 375; //miles driven and gallons used given in problem statement
+  float milesDriven = 375; 
   float gallonsUsed = 15;
-  float milesPerGallon = milesDriven / gallonsUsed; //formula for miles per gallon
 
-  cout << "After using " << gallonsUsed << "gal and traveling " << milesDriven << "mi your miles per gallon is " << milesPerGallon << "mpg. "; //outputs the information
+  cout << "How many gallons of gas did you use? ";
+  cin >> gallonsUsed;
+
+  cout << "How many miles did you travel? ";
+  cin >> milesDriven;
+  
+  float milesPerGallon = milesDriven / gallonsUsed; //formula for miles per gallon
+  
+  cout << "After using " << gallonsUsed << " gal and traveling " << milesDriven << " mi your miles per gallon is " << milesPerGallon << " mpg. "; //outputs the information
 }
